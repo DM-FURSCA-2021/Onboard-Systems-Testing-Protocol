1cf62e07c9b36da472ad88f02b4ba80458811271
Daniel-McGarry <85240844+Daniel-McGarry@users.noreply.github.com>
2021-06-07 17:23:33 -0400

    Add files via upload

---
+++ Combined_Sensor_Test.ino
@@ -0,0 +1,208 @@
+//BMP
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_BMP280.h>
+
+//Servos
+#include <Servo.h>
+
+//BNO
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
+
+Adafruit_BMP280 bmp; // I2C
+
+int servoPin1=7;
+int servoPos1=90;
+
+int servoPin2=6;
+int servoPos2=90;
+
+Servo myServo1;
+Servo myServo2;
+
+uint16_t BNO055_SAMPLERATE_DELAY_MS = 100;
+
+Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x29);
+
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Initializing Combined Systems Testing Protocol");
+  delay(500);
+  
+  //BMP Test
+  Serial.println(F("BMP280 Sensor Test"));
+  delay(100);
+  
+  //if (!bmp.begin(BMP280_ADDRESS_ALT, BMP280_CHIPID)) {
+  if (!bmp.begin()) {
+    Serial.println(F("Could not find a valid BMP280 sensor, check wiring or "
+                      "try a different address!"));
+    while (1) delay(10);
+  }
+
+  /* Default settings from datasheet. */
+  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
+                  Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
+                  Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
+                  Adafruit_BMP280::FILTER_X16,      /* Filtering. */
+                  Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
+
+    Serial.print(F("Temperature = "));
+    Serial.print(bmp.readTemperature());
+    Serial.println(" *C");
+
+    Serial.print(F("Pressure = "));
+    Serial.print(bmp.readPressure());
+    Serial.println(" Pa");
+
+    Serial.print(F("Approx altitude = "));
+    Serial.print(bmp.readAltitude(1013.25)); /* Adjusted to local forecast! */
+    Serial.println(" m");
+
+    Serial.println();
+    delay(2000);
+
+
+  //BNO Test
+  Serial.println(F("BNO055 Sensor Test"));
+  delay(100);  
+  /* Initialise the sensor */
+  if (!bno.begin())
+  {
+    /* There was a problem detecting the BNO055 ... check your connections */
+    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+    while (1);
+  }
+
+  delay(1000);
+
+  sensors_event_t orientationData , angVelocityData , linearAccelData, magnetometerData, accelerometerData, gravityData;
+  bno.getEvent(&orientationData, Adafruit_BNO055::VECTOR_EULER);
+  bno.getEvent(&angVelocityData, Adafruit_BNO055::VECTOR_GYROSCOPE);
+  bno.getEvent(&linearAccelData, Adafruit_BNO055::VECTOR_LINEARACCEL);
+  bno.getEvent(&magnetometerData, Adafruit_BNO055::VECTOR_MAGNETOMETER);
+  bno.getEvent(&accelerometerData, Adafruit_BNO055::VECTOR_ACCELEROMETER);
+  bno.getEvent(&gravityData, Adafruit_BNO055::VECTOR_GRAVITY);
+  
+  printEvent(&orientationData);
+  printEvent(&angVelocityData);
+  printEvent(&linearAccelData);
+  printEvent(&magnetometerData);
+  printEvent(&accelerometerData);
+  printEvent(&gravityData);
+  
+  int8_t boardTemp = bno.getTemp();
+  Serial.println();
+  Serial.print(F("temperature: "));
+  Serial.println(boardTemp);
+  
+  uint8_t system, gyro, accel, mag = 0;
+  bno.getCalibration(&system, &gyro, &accel, &mag);
+  Serial.println();
+  Serial.print("Calibration: Sys=");
+  Serial.print(system);
+  Serial.print(" Gyro=");
+  Serial.print(gyro);
+  Serial.print(" Accel=");
+  Serial.print(accel);
+  Serial.print(" Mag=");
+  Serial.println(mag);
+  
+  Serial.println("--");
+  delay(BNO055_SAMPLERATE_DELAY_MS);
+
+
+
+
+
+  //Servo Setup
+  myServo1.attach(servoPin1);
+  myServo2.attach(servoPin2);
+
+  
+  Serial.println("Testing Servo 1");
+  delay(500);
+
+  myServo1.write(servoPos1);
+  delay(500);
+  myServo1.write(135);
+  delay(250);
+  myServo1.write(45);
+  delay(250);
+  myServo1.write(90);
+  delay(1000);
+
+  
+  Serial.println("Testing Servo 2");
+  delay(500);
+  
+  myServo2.write(servoPos2);
+  delay(500);
+  myServo2.write(135);
+  delay(250);
+  myServo2.write(45);
+  delay(250);
+  myServo2.write(90);
+  delay(1000);
+
+  Serial.println("Systems Testing Protocol Complete");
+                  
+}
+
+void loop() {
+
+
+}
+//BNO printEvent
+void printEvent(sensors_event_t* event) {
+  double x = -1000000, y = -1000000 , z = -1000000; //dumb values, easy to spot problem
+  if (event->type == SENSOR_TYPE_ACCELEROMETER) {
+    Serial.print("Accl:");
+    x = event->acceleration.x;
+    y = event->acceleration.y;
+    z = event->acceleration.z;
+  }
+  else if (event->type == SENSOR_TYPE_ORIENTATION) {
+    Serial.print("Orient:");
+    x = event->orientation.x;
+    y = event->orientation.y;
+    z = event->orientation.z;
+  }
+  else if (event->type == SENSOR_TYPE_MAGNETIC_FIELD) {
+    Serial.print("Mag:");
+    x = event->magnetic.x;
+    y = event->magnetic.y;
+    z = event->magnetic.z;
+  }
+  else if (event->type == SENSOR_TYPE_GYROSCOPE) {
+    Serial.print("Gyro:");
+    x = event->gyro.x;
+    y = event->gyro.y;
+    z = event->gyro.z;
+  }
+  else if (event->type == SENSOR_TYPE_ROTATION_VECTOR) {
+    Serial.print("Rot:");
+    x = event->gyro.x;
+    y = event->gyro.y;
+    z = event->gyro.z;
+  }
+  else if (event->type == SENSOR_TYPE_LINEAR_ACCELERATION) {
+    Serial.print("Linear:");
+    x = event->acceleration.x;
+    y = event->acceleration.y;
+    z = event->acceleration.z;
+  }
+  else {
+    Serial.print("Unk:");
+  }
+
+  Serial.print("\tx= ");
+  Serial.print(x);
+  Serial.print(" |\ty= ");
+  Serial.print(y);
+  Serial.print(" |\tz= ");
+  Serial.println(z);
+}
